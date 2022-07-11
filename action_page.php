a0b162bd4c996cf34f5629b2375249c0f0d5eed2
skyelite1 <108718601+skyelite1@users.noreply.github.com>
2022-07-11 08:25:19 +0700

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style3.css">
+<title>XI_PPLG</title>
+</head>
+<body>
+<div class="header" id="header">
+ <h1>Welcome</h1>
+</div>
+
+<ul class="topnav">
+  <li><a class="active" href="action_page.php">Home</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Contact</a>
+    <div class="dropdown-content">
+      <a href="https://www.instagram.com/85dulloh/">Instagram</a>
+      <a href="https://api.whatsapp.com/send/?phone=%2B6285693332869&text&type=phone_number&app_absent=0">WhatsApp</a>
+  <li class="right" id="login"><a href="project.html">Sign in</a></li>
+</ul>
+
+<div >
+    <h1 id="judul">RPL/PPLG</h1>
+    <h2 id="soal">Apa Itu RPL?</h2>
+    <H3 id="isi">RPL adalah singkatan dari Rekayasa Perangkat Lunak dan merupakan sebuah jurusan yang ada di Sekolah Menengah Kejuruan (SMK). RPL adalah sebuah jurusan yang mempelajari dan mendalami semua cara-cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas.
+Bukan hanya itu, RPL juga berkaitan dengan software komputer mulai dari pembuatan website, aplikasi, game dan semua yang berkaitan dengan pemrograman dengan menguasai bahasa pemrograman tersebut. Intinya RPL tidak akan jauh-jauh dari tiga hal yaitu Coding, Desain dan Algoritma yang akan menjadi kunci keberhasilan rekayasa perangkat lunak tersebut.</H3>
+    <h2 id="soal">Apa Saja Keunggulan Jurusan RPL?</h2>
+    <h3 id="isi">1. Mampu bekerja di berbagai bidang karena sudah dibekali dengan berbagai ilmu dan pengetahuan.</h3>
+    <h3 id="isi">2. Dalam melakukan kerja lapangan akan lebih mudah karena saat pembelajaran sudah sering melakukan kerja praktek.</h3>
+    <h3 id="isi">3. Pekerjaan nya yang relatif mudah dan santai, dapat dikerjakan dimanapun dan kapanpun menggunakan koneksi tentunya.</h3>
+    <h3 id="isi">4. Lebih terdepan dari jurusan lainnya dan orang awam diluar sana karena jurusan RPL lebih mengerti dan mendalami berbagai teknologi.</h3>
+    <h3 id="isi">5. Mampu memasang sebuah PC.</h3>
+    <h3 id="isi">6. Mengerti akan fungsi dari komponen komputer.</h3>
+    <h2 id="soal">Apa Saja Yang Dipelajari Di Jurusan RPL?</h2>
+    <h3 id="isi">Sebetulnya RPL (Rekayasa Perangkat Lunak) itu Gampang ! tidak jauh jauh sama 3 hal ini yaitu:</h3>
+    <h3 id="isi">1. Coding</h3>
+    <h3 id="isi">2. Desain</h3>
+    <h3 id="isi">3. Algoritma</h3>
+    <h3 id="isi">Didalam Coding, Desain dan Algoritma tersebut masih ada Point-Point penting RPL (Rekayasa Perangkat Lunak). Inilah point-point tersebut :</h3>
+    <h2 id="isi">1. Coding</h2>
+    <h3 id="isi">– Pemograman Bahasa Pascal</h3>
+    <h3 id="isi">– Pemograman Bahasa C</h3>
+    <h3 id="isi">– Pemograman Bahasa C++</h3>
+    <h3 id="isi">– Pemograman Bahasa Java</h3>
+    <h3 id="isi">– Pemograman Bahasa Python (*)</h3>
+    <h3 id="isi">– Pemograman Bahasa Delphi (*)</h3>
+    <h3 id="isi">– Pemograman PHP & Mysql</h3>
+    <h3 id="isi">– Pemograman JavaScript</h3>
+    <h3 id="isi">– Pemograman AJAX (*)</h3>
+    <h3 id="isi">– Pemograman Web Server</h3>
+    <h3 id="isi">– Pemograman HTML</h3>
+    <h3 id="isi">– Pemograman CSS</h3>
+    <h3 id="isi">– Dan masih banyak lagi</h3>
+    <h2 id="isi">2. Desain</h2>
+    <h3 id="isi">– Photoshop</h3>
+    <h3 id="isi">– Corel Draw</h3>
+    <h3 id="isi">– Video Editing</h3>
+    <h3 id="isi">– Web Design</h3>
+    <h3 id="isi">– Dan masih banyak lagi</h3>
+    <h2 id="isi">3. Algoritma</h2>
+    <h3 id="isi">– Algoritma Dasar</h3>
+    <h3 id="isi">– Algoritma tingkat Lanjut</h3>
+    <h3 id="isi">– Microsoft Access</h3>
+    <h3 id="isi">– Gerbang Logika</h3>
+    <h3 id="isi">– Basis Data</h3>
+    <h3 id="isi">– DFD (Data Flow Diagram)</h3>
+    <h3 id="isi">– Dan masih banyak lagi</h3>
+    <h3 id="isi">Biasanya selain itu jurusan RPL juga mempelajari tentang Perakitan Komputer, Jaringan Dasar dan masih banyak lagi. Kenapa selalu ada masih banyak lagi? karena jurusan RPL ini bersinggungan dengan teknologi sehingga perkembanganya mengikuti. Jadi, kesimpulannya RPL (Rekayasa Perangkat Lunak) itu tidak sekedar Membuat Program (Software) dan Membuat Web saja. RPL (Rekayasa Perangkat Lunak) Itu Luas.</h3>
+    <h2 id="soal">Jurusan RPL Setelah Lulus Bisa Kerja Apa?</h2>
+    <h3 id="isi">Banyak yang tanya jurusan RPL itu nanti kalau lulus bisa kerja apa? apa saja lowongan kerja lulusan SMK jurusan RPL. Berikut daftar peluang kerja jurusan RPL:</h3>
+    <h2 id="isi">1. Konfersi PSD ke WordPress</h2>
+    <h3 id="isi">Merupakan pekerjaan yang berperan untuk anda agar mampu memenuhi kebutuhan klien salah satunya mengubah hasil photoshop menjadi file wordpress.</h3>
+    <h2 id="isi">2. Developer IT</h2>
+    <h3 id="isi">Merupakan pekerjaan yang mengharuskan anda dapat mengembangkan perangkat lunak dengan memenuhi kebutuhan klien. Seperti perangkat lunak untuk pendidikan, bisnis, hiburan games, telekomunikasi dll.</h3>
+    <h2 id="isi">3. Programmer</h2>
+    <h3 id="isi">Membuat program sebagau perangkat lunak dari komputeer untuk memudahkan fungsi tertentu. Programmer dapat bekerja di segala bidang baik industri, pertelevisan, perbankan, media. Jadi peluang kerja jurusan rpl sangat banyak.</h3>
+    <h2 id="isi">4. IT colsultant</h2>
+    <h3 id="isi">Anda bisa bekerja sebagai IT consultant yang beperan dalam perencanaan dan pengevaluasian penerapan IT pada sebuah organisasi. Jadi lowongan kerja smk jurusan rpl banyak, tinggal anda mencari informasinya.</h3>
+    <h2 id="isi">5. System Analyst dan system integrator</h2>
+    <h3 id="isi">Yang nantinya anda harus berperan dalam melakukan analisis terhadap sistem atau program yang berbasis teknologi dan informasi dalam suatu perusahaan dan membuat solusi yang integrasi dan memanfaatkan perangkat lunak. Ini menjadi salah satu peluang kerja lulusan smk jurusan rpl.</h3>
+    <h2 id="isi">6. Database engineer / data base administrator</h2>
+    <h3 id="isi">Berperan dalam perencanaan pemeliharaan basis data (termasuk data warehouse)</h3>
+    <h2 id="isi">7. Web engineer</h2>
+    <h3 id="isi">Bertugas merancang dan membangun website beserta layanan dan fasilitas berjalan di atasnya dan bertanggung jawab dalam pemeliharaan website dan mengembangkannya,</h3>
+    <h2 id="isi">8. Computer network / data communication engineer</h2>
+    <h3 id="isi">Bertugas untuk merancang arsitektur jaringan komputer dan melakukan perawatan, pengelolaan jaringan dalam instansi atau perusahaan.</h3>
+    <h2 id="isi">9. Game developer</h2>
+    <h3 id="isi">Anda diharuskan dapat mengembangkan perangkat lunak multimedia game dan</h3>
+    <h2 id="isi">10. Intellegent system developer</h2>
+    <h3 id="isi">Mengembangkan perangkat lunak yang intelegen seperti sistem pakar, image recognizer dll.</h3>
+    <h2 id="isi">11. Software tester</h2>
+    <h3 id="isi">Anda diharuskan dapat berperan khusus sebagai penguji perangkat lunak dan bertanggung jawab atas kebenaran fungsi dari perangkat lunak maupun dari sistem.</h3>
+</div>
+</body>
+</html>
